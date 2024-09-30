12d4157a990550e9d8669cf809cca3f829dc0b2f
realestestate <>
2024-09-29 20:27:10 -0700

    populated main

--- main.cpp
+++ main.cpp
@@ -8,18 +8,19 @@ class Color{
     int r;
     int g;
     int b;
-    //constructor w/ setters
-    Color(int R, int G, int B){setR(R);setG(G);setB(B);}
     //setters
     void setR(int R){r=R;}
     void setG(int G){g=G;}
-    void setB(int B){n=B;}
+    void setB(int B){b=B;}
     //getters
     int getR(){return r;}
     int getG(){return g;}
     int getB(){return b;}
     //print method
-    void print(){
+    public:
+    //constructor
+    Color(int R, int G, int B){setR(R);setG(G);setB(B);}
+    void printColor(){
         cout<<"R: "<<r<<endl;
         cout<<"G: "<<g<<endl;
         cout<<"B: "<<b<<endl;
@@ -27,5 +28,17 @@ class Color{
 };
 
 int main(){
+    Color *C1 = new Color(1,2,3);
+    Color *C2 = new Color(1,2,3);
+    Color *C3 = new Color(1,2,3);
+    Color *C4 = new Color(1,2,3);
+    C1->printColor();
+    C2->printColor();
+    C3->printColor();
+    C4->printColor();
+    delete C1;
+    delete C2;
+    delete C3;
+    delete C4;
     return 0;
 }
