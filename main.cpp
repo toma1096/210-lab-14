595edc87dcc64fe703dd1dede23e050ec94f311c
realestestate <>
2024-09-29 20:29:42 -0700

    Cleaned up output and functions

--- main.cpp
+++ main.cpp
@@ -24,17 +24,22 @@ class Color{
         cout<<"R: "<<r<<endl;
         cout<<"G: "<<g<<endl;
         cout<<"B: "<<b<<endl;
+        cout<<"====================\n";
     }
 };
 
 int main(){
     Color *C1 = new Color(1,2,3);
-    Color *C2 = new Color(1,2,3);
-    Color *C3 = new Color(1,2,3);
-    Color *C4 = new Color(1,2,3);
+    Color *C2 = new Color(100,222,30);
+    Color *C3 = new Color(29,80,240);
+    Color *C4 = new Color(190,5,77);
+    cout<<"Color 1\n";
     C1->printColor();
+    cout<<"Color 2\n";
     C2->printColor();
+    cout<<"Color 3\n";
     C3->printColor();
+    cout<<"Color 4\n";
     C4->printColor();
     delete C1;
     delete C2;
