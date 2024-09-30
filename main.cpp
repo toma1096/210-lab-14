3c862f8b9def35e064ce0e39c2bd8978e873945e
realestestate <>
2024-09-29 20:21:03 -0700

    progress on Color Class

--- main.cpp
+++ main.cpp
@@ -4,21 +4,28 @@
 using namespace std;
 
 class Color{
+    //members
     int r;
     int g;
     int b;
-
-    Color(int R, int G, int B){r=R;g=G;b=B;}
-
+    //constructor w/ setters
+    Color(int R, int G, int B){setR(R);setG(G);setB(B);}
+    //setters
+    void setR(int R){r=R;}
+    void setG(int G){g=G;}
+    void setB(int B){n=B;}
+    //getters
+    int getR(){return r;}
+    int getG(){return g;}
+    int getB(){return b;}
+    //print method
     void print(){
-        cout<<"R: "<<r;
-        cout<<"G: "<<g;
-        cout<<"B: "<<b;
+        cout<<"R: "<<r<<endl;
+        cout<<"G: "<<g<<endl;
+        cout<<"B: "<<b<<endl;
     }
 };
 
 int main(){
-    
-    
     return 0;
 }
